30d6cf44998e4a062a95df6df31efc1417237bf6
bu-lo <chloe.boivin@outlook.com>
2024-07-31 10:47:41 +0200

    Declaration of all elements @FXML

--- javacard/src/main/java/fr/afpa/Controllers/UserManagerController.java
+++ javacard/src/main/java/fr/afpa/Controllers/UserManagerController.java
@@ -7,5 +7,73 @@ import javafx.fxml.FXML;
 
 public class UserManagerController {
 
+    @FXML
+    private Button deleteButton
+
+    @FXML
+    private Button newButton
+    
+    @FXML
+    private Button exportButton
+
+    @FXML
+    private Button saveButton
+
+    @FXML
+    private TextField textFieldFirstName
+
+    @FXML
+    private TextField textFieldLastName
+
+    @FXML
+    private TextField textFieldPseudo
+
+    @FXML
+    private TextField textFieldAddress
+
+    @FXML
+    private TextField textFieldNumber
+
+    @FXML
+    private TextField textFieldProNumber
+
+    @FXML
+    private TextField textFieldMail
+
+    @FXML
+    private TextField textFieldLinkedin
+
+    @FXML
+    private TextField textFieldGitHub
+
+    @FXML
+    private ComboBox comboBoxGender
+
+    @FXML
+    private ComboBox comboBoxSelectFormat
+
+    @FXML
+    private DatePicker datePickerBirthday
+
+    @FXML
+    private CheckBox checkBoxSelectAll
+
+    @FXML
+    private TableView tableView4columns
+
+    @FXML
+    private TableColumn columnFirstName
+
+    @FXML
+    private TableColumn columnLastName
+
+    @FXML
+    private TableColumn columnNumber
+
+    @FXML
+    private TableColumn columnMail
+
+
+
     
 }
