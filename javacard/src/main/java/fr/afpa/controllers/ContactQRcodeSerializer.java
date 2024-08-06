49f2821f00c5027c0e7211e568ab7a72a6fd5148
Marchive Florian <marchiveflorian@gmail.com>
2024-08-06 09:30:28 +0200

    QRcode works

--- javacard/src/main/java/fr/afpa/controllers/ContactQRcodeSerializer.java
+++ javacard/src/main/java/fr/afpa/controllers/ContactQRcodeSerializer.java
@@ -1,64 +1,94 @@
 package fr.afpa.controllers;
 
-// Java code to generate QR code
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+// Importing necessary libraries for QR code generation
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContactQRcodeSerializer {
 
-    // Function to create the QR code
+    /**
+     * Creates a QR code and saves it to the specified path.
+     *
+     * @param data     The data to be encoded in the QR code
+     * @param path     The file path where the QR code image will be saved
+     * @param charset  The character set to be used
+     * @param hashMap  Map containing QR code encoding parameters
+     * @param height   The height of the QR code image
+     * @param width    The width of the QR code image
+     * @throws WriterException If an error occurs during the writing process
+     * @throws IOException     If an I/O error occurs
+     */
     public static void createQR(String data, String path,
-                                String charset, Map hashMap,
+                                String charset, Map<EncodeHintType, ErrorCorrectionLevel> hashMap,
                                 int height, int width)
-        throws WriterException, IOException
-    {
+            throws WriterException, IOException {
 
+        // Encoding the data using the specified charset and generating a QR code matrix
         BitMatrix matrix = new MultiFormatWriter().encode(
-            new String(data.getBytes(charset), charset),
-            BarcodeFormat.QR_CODE, width, height);
+                new String(data.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, width, height);
 
-        MatrixToImageWriter.writeToFile(
-            matrix,
-            path.substring(path.lastIndexOf('.') + 1),
-            new File(path));
+        // Defining the path to save the QR code image
+        Path pathFile = Paths.get(path);
+
+        // Writing the QR code matrix to an image file
+        MatrixToImageWriter.writeToPath(
+                matrix,
+                path.substring(path.lastIndexOf('.') + 1),
+                pathFile);
     }
-  
-    // Driver code
+
+    /**
+     * Reads the content of a file and returns it as a string.
+     *
+     * @param filePath The path of the file to be read
+     * @return The content of the file as a string
+     * @throws IOException If an I/O error occurs
+     */
+    public static String readFileAsString(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    /**
+     * Main method to generate a QR code from a vCard file.
+     *
+     * @param args Command line arguments
+     * @throws WriterException If an error occurs during the writing process
+     * @throws IOException     If an I/O error occurs
+     */
     public static void main(String[] args)
-        throws WriterException, IOException,
-               NotFoundException
-    {
+            throws WriterException, IOException {
 
-        // The data that the QR code will contain
-        String data = "www.geeksforgeeks.org";
+        // The path to the vCard file
+        String vcardFilePath = "contacts.vcf";
 
-        // The path where the image will get saved
-        String path = "QRcode.png";
+        // Reading the vCard file content
+        String vcardString = readFileAsString(vcardFilePath);
+
+        // The path where the QR code image will be saved
+        String qrCodeImagePath = "QRcode.png";
 
         // Encoding charset
         String charset = "UTF-8";
 
-        Map<EncodeHintType, ErrorCorrectionLevel> hashMap
-            = new HashMap<EncodeHintType,
-                          ErrorCorrectionLevel>();
+        // Creating a map to define the QR code error correction level
+        Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<>();
+        hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 
-        hashMap.put(EncodeHintType.ERROR_CORRECTION,
-                    ErrorCorrectionLevel.L);
-
-        // Create the QR code and save
-        // in the specified folder
-        // as a jpg file
-        createQR(data, path, charset, hashMap, 200, 200);
+        // Creating the QR code and saving it as a PNG file
+        createQR(vcardString, qrCodeImagePath, charset, hashMap, 200, 200);
         System.out.println("QR Code Generated!!! ");
     }
 }
--- javacard/src/main/java/module-info.java
+++ javacard/src/main/java/module-info.java
@@ -3,8 +3,8 @@ module fr.afpa {
     requires javafx.fxml;
     requires ez.vcard;
     requires json.simple;
-    requires core;
-    requires javase;
+    requires com.google.zxing.javase;
+    requires com.google.zxing;
 
     opens fr.afpa to javafx.fxml;
     opens fr.afpa.controllers to javafx.fxml;
